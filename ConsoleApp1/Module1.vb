542e12a3ca4b7e486687fe76bb04aa70b0e92a05
UlyssesCat <919504937@qq.com>
2018-10-22 22:41:14 +0800

    添加项目文件。

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim o = CreateObject("MyLib.MyClass")
+        o.Initialize
+        Console.WriteLine("1 + 2 = " & o.Add(1, 2))
+        o.Dispose
+        o = Nothing
+    End Sub
+
+End Module
---
+++ TestLib/Program.cs
@@ -0,0 +1,33 @@
+﻿using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMyClass myClass = CreateMyClass();
+            Console.WriteLine(myClass.Add(3,4));
+        }
+        public static IMyClass CreateMyClass()
+        {
+            IMyClass iMyClass = null;
+            try
+            {
+                Guid guid = new Guid("F0239BF9-0A6E-49A6-8853-BADE1B95E66F");
+                Type myClassType = Type.GetTypeFromCLSID(guid);
+                object myClass = Activator.CreateInstance(myClassType);
+                iMyClass = myClass as IMyClass;
+            }
+            catch (Exception e)
+            {
+            }
+            return iMyClass;
+        }
+    }
+}
